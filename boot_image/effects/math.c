e345e3cdb9cceedf7b81f04158b5db75494e7531
Binyamin Sharet <s.binyamin@gmail.com>
2013-10-09 20:34:50 +0300

    Fixed negative detection

--- boot_image/effects/math.c
+++ boot_image/effects/math.c
@@ -4,7 +4,7 @@
 int scaled_sine_8_bit(const unsigned int degrees)
 {
     int tmp = (int)(degrees & 0x7f);
-    unsigned int is_negative = (degrees * 0x80);
+    unsigned int is_negative = (degrees & 0x80);
 
     tmp *= (0x80-tmp);
     tmp /= 32;
