d45cb10771161832f1ed6c2a751c4833db9f8e2f
Jon Ronen-Drori <jon_ronen@yahoo.com>
2026-04-10 10:46:09 +0300

    Generalize octaver to a pitch shifter
    Note: pitch shift doesn't work well with non-octave at the moment...

--- scripts/wav_pitch_low_shift.py
+++ scripts/wav_pitch_low_shift.py
@@ -3,39 +3,76 @@ import struct, sys, math, numpy, functools
 from scipy.fft import fft, ifft
 from optparse import OptionParser
 from collections import namedtuple
+import matplotlib.pyplot as plt
 
 
 class OctaverContext:
-    def __init__(self, sample_cnt, bits):
+    def __init__(self, sample_cnt, bits, scale_factor):
         # sanity checks
         if bits not in [8,16,24,32]: raise ValueError()
         if sample_cnt not in [128,256,512,1024,2048]: raise ValueError()
+        if scale_factor > 2 or scale_factor < 0.5: raise ValueError()
         
         # sizes and lengths
         self.sample_cnt = sample_cnt
         self.bits = bits
         self.max_sample = 2**(bits-1)
+        self.scale_factor = scale_factor
+        self.frame_idx = 0
         
         #
-        # some frequency bins require different handling according to the frame index.
-        # save it and increment it when handling every frame
+        # save the last phase shift of the output signal, in order to make the phase continuous
+        # this is saved per frequency bin
         #
-        self.last_input_idx = 0
+        self.output_phase_shifts = numpy.array([1] * sample_cnt, dtype = complex)
         
         # window manipulations for smoother signals
         self.hann_window = numpy.array([math.sin(math.pi*n/sample_cnt)**2 for n in range(sample_cnt)])
-        self.hann_window_double = numpy.array([math.sin(math.pi*n/sample_cnt/2)**2 for n in range(sample_cnt*2)])
+        self.hann_window_scaled = \
+            numpy.array([math.sin(math.pi * n / sample_cnt / scale_factor)**2 \
+                for n in range(math.ceil(sample_cnt * scale_factor))])
         
         # context variables for each window: previous clean buffer, previous four modified buffers
         self.prev_buff = numpy.array([0] * sample_cnt, dtype = complex)
-        self.prev_mod_buffs = [numpy.array([0] * sample_cnt * 2, dtype = complex) for i in range(4)]
+        self.prev_mod_buffs = [numpy.array([0] * int(math.ceil(sample_cnt * self.scale_factor)), dtype = complex) for i in range(4)]
     
-    def extrapolate(buff):
-        return numpy.array([buff[i//2] for i in range(2*buff.size)])
+    def extrapolate(buff, extrapolation_factor):
+        new_size = math.ceil(buff.size * extrapolation_factor)
+        
+        # result is a bigger array, according to the extrapolation factor
+        res = numpy.array([0] * new_size, dtype=complex)
+        
+        # we iterate over the indices, using a linear extrapolation
+        idx_inc = 1 / extrapolation_factor
+        idx_curr = 0
+        
+        for idx in range(new_size):
+            target_idx = idx * idx_inc
+            if target_idx % 1 == 0:
+                res[idx] = buff[int(target_idx)]
+            elif int(target_idx) >= buff.size - 1:
+                res[idx] = buff[buff.size - 1]
+            else:
+                low_num = buff[int(target_idx)]
+                high_num = buff[int(target_idx) + 1]
+                frac = target_idx % 1
+                res[idx] = low_num * (1 - frac) + high_num * frac
+        
+        return res
     
     def get_windowed_quarters(self, buff, start_quarter):
-        l = numpy.array(([0] * (self.sample_cnt//2)) + list(buff * self.hann_window_double) + ([0] * (self.sample_cnt//2)))
-        return l[(start_quarter + 1) * self.sample_cnt//2 : (start_quarter + 3) * self.sample_cnt//2]
+        #
+        # since the buffers are shifted by half the sampling window, we want to pad them in order to be able to take
+        # just the quarters we need from them
+        #
+        l = [0] * int(self.sample_cnt // 2) # half-window padding
+        l += list(buff * self.hann_window_scaled) # scaled sample
+        l += [0] * (self.sample_cnt * 2 - self.hann_window_scaled.size) # padding to a double window
+        l += [0] * int(self.sample_cnt // 2) # padding of another half-window
+        l = numpy.array(l)
+        
+        # get the two relevant half-window chunks to form a window-sized buffer
+        return l[(start_quarter + 1) * int(self.sample_cnt // 2) : (start_quarter + 3) * int(self.sample_cnt // 2)]
     
     def process_buff(self, curr_buff):
         # start with a copy of the new buffer, in the form of numpy.ndarray
@@ -53,16 +90,25 @@ class OctaverContext:
         
         # compute the phase shift of each (non-DC) frequency bin and align the output phase accordingly
         for k in range(1, self.sample_cnt):
-            phase_shift = numpy.exp(-1j * numpy.pi / 2 * (k % 4))
-            mid_buff_fft[k] *= phase_shift
-            new_buff_fft[k] *= (phase_shift**2)
-            if (self.last_input_idx % 2) == 0:
-                mid_buff_fft[k] *= (phase_shift**2)
-                new_buff_fft[k] *= (phase_shift**2)
+            phase_shift = numpy.exp(1j * numpy.pi / self.scale_factor * k)
+            mid_buff_fft[k] *= self.output_phase_shifts[k] * phase_shift
+            if k % 2 != 0:
+                mid_buff_fft[k] *= -1
+            new_buff_fft[k] *= self.output_phase_shifts[k] * (phase_shift**2)
+            self.output_phase_shifts[k] *= (phase_shift**2)
         
         # stretch the arrays
-        new_mod_buff = OctaverContext.extrapolate(ifft(new_buff_fft))
-        mid_mod_buff = OctaverContext.extrapolate(ifft(mid_buff_fft))
+        new_mod_buff = OctaverContext.extrapolate(ifft(new_buff_fft), self.scale_factor)
+        mid_mod_buff = OctaverContext.extrapolate(ifft(mid_buff_fft), self.scale_factor)
+        
+        # if self.frame_idx < 10:
+        #     plt.plot(list(range(self.sample_cnt)), self.get_windowed_quarters(self.prev_mod_buffs[0], 3))
+        #     plt.plot(list(range(self.sample_cnt)), self.get_windowed_quarters(self.prev_mod_buffs[1], 2))
+        #     plt.plot(list(range(self.sample_cnt)), self.get_windowed_quarters(self.prev_mod_buffs[2], 1))
+        #     plt.plot(list(range(self.sample_cnt)), self.get_windowed_quarters(self.prev_mod_buffs[3], 0))
+        #     plt.plot(list(range(self.sample_cnt)), self.get_windowed_quarters(mid_mod_buff, -1))
+        #     plt.grid()
+        #     plt.show()
         
         # combine the different windows from each array
         res = self.get_windowed_quarters(self.prev_mod_buffs[0], 3)
@@ -77,7 +123,8 @@ class OctaverContext:
         self.prev_mod_buffs[1] = self.prev_mod_buffs[3]
         self.prev_mod_buffs[2] = mid_mod_buff
         self.prev_mod_buffs[3] = new_mod_buff
-        self.last_input_idx += 1
+        self.frame_idx += 1
+        
         return list(map(lambda x: int(x.real), res * self.max_sample))
 
 
@@ -86,6 +133,7 @@ if __name__ == "__main__":
     parser.add_option("-o", "--out", dest="outfile", help="output filename")
     parser.add_option("-b", "--buff-size", dest="buff_size", type=int, default=1024, help="buffer size (default: 1024)")
     parser.add_option("-p", "--plot-frames", dest="plot_frames", type=int, default=0, help="plot the first frames (default: 0)")
+    parser.add_option("-s", "--semitones", dest="semitones", type=int, default=12, help="semitones for pitch shift (default: 12)")
 
     (options, args) = parser.parse_args()
     if len(args) < 1:
@@ -119,11 +167,11 @@ if __name__ == "__main__":
         print("input file is not a wav file")
         sys.exit()
     
-    contexts = [OctaverContext(options.buff_size, unpacked_hdr.bits), OctaverContext(options.buff_size, unpacked_hdr.bits)]
+    scale_factor = 2**(options.semitones / 12)
+    contexts = [OctaverContext(options.buff_size, unpacked_hdr.bits, scale_factor),
+                OctaverContext(options.buff_size, unpacked_hdr.bits, scale_factor)]
     
     data_len = unpacked_hdr.data_len
-    
-    import matplotlib.pyplot as plt
     
     frame_idx = 0
     
